d00037fc5353d2bfaac37ba7ba3e43181073b1ae
Dmitriy Istomin <dmi.istomin@gmail.com>
2016-07-23 22:53:41 -0400

    add example test, test config, remove .keep file

---
+++ config/webpack.test.config.js
@@ -0,0 +1,15 @@
+module.exports = {
+  target: 'node',
+  module: {
+    loaders: [
+      {
+        test: /\.js$/,
+        loader: 'babel',
+        query: {
+          presets: ['es2015']
+        },
+        exclude: /node_modules/
+      }
+    ]
+  }
+};
---
+++ spec/spec.js
@@ -0,0 +1,12 @@
+import { chai, expect } from 'chai';
+
+import Foo from '../src/Foo';
+
+describe('an example test', () => {
+  describe('#bar', () => {
+    it('returns a string', () => {
+      expect(new Foo().bar()).to.equal('ES2015 compilation works properly!');
+    })
+  })
+})
+
